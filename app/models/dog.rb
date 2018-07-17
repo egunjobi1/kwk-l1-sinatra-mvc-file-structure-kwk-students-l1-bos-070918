43e4939d338212f3d89ea0006541b9a04f869c7e
Oluwapelumi Egunjobi <pelumitegunjobi@gmail.com>
2018-07-17 14:48:33 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,14 +2,16 @@ class Dog
   
   attr_accessor :name, :breed, :age
   
-  @@dog_count = [ ]
+  @@dog_all = [ ]
     
   def initialize(name,breed,age)
     @name = name
     @age = age
+    @@dog_all << self
   end
 end
 
   def self.all
-    @@dog_count
+    @@dog_all
   end
+end
